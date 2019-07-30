282f953cff94bc31b1e4ae747d76e2b54f3ab897
May <may159@csiro.au>
2019-07-30 11:25:42 +1000

    put the slider in

--- Dash.R
+++ Dash.R
@@ -25,29 +25,54 @@ ui <- dashboardPage(
       #load menu items
       sidebarMenu(
         menuItem("Landing", tabName = "Landing", icon = icon("paper-plane")),
-        menuItem("15min", tabName = "first", icon = icon("dashboard")),
+        menuItem("15min", tabName = "rows", icon = icon("dashboard")),
         menuItem("summ_daily", tabName = "second", icon = icon("th")),
         menuItem("summ_yield", tabName = "yield", icon = icon("chart-bar"))
       )
     ),
   dashboardBody(
     #load tab items frmo the menuitem
-    (tabItems(tabItem(tabName  = "first",
-                      h2("Select Data to View")
+    (tabItems(tabItem(tabName  = "Landing",
+                      h2("Select 15 minute Data to View"),
+      fluidRow(
+                column(4,
+                               
+                         # Copy the line below to make a slider bar 
+                               sliderInput("slider1", label = h3("Slider"), min = 0, 
+                                 max = 100, value = 50)
+                        ),
+                        column(4,
+                               
+                               # Copy the line below to make a slider range 
+                          sliderInput("slider2", label = h3("Slider Range"), min = 0, 
+                                     max = 100, value = c(40, 60))
+                        )
+                      ),
+                      
+                      hr(),
+                      
+                      fluidRow(
+                        column(4, verbatimTextOutput("value")),
+                        column(4, verbatimTextOutput("range"))
+                      )
+    )                 
+    )
     ),
-    tabItem(tabName = "15 minute Canopy Temperature",
-            h2("second tab content")
-    )
-    )
-    )
     
-  )
+    tabItem(tabName = "rows",
+            h2("select rows")
+    )
+    )
+    )
+  
+
+
       
       # Boxes need to be put in a row (or column)
-      #fluidRow(
-       # column(width = 12,
-       #        h2("Please select data source")
-        #       ),
+      fluidRow(
+       column(width = 12,
+               h2("Please select data source")
+              ),
       
         #box(width =8,plotOutput("plot1", height = 250)),
         
@@ -68,6 +93,14 @@ ui <- dashboardPage(
       data <- histdata[seq_len(input$slider)]
       hist(data)
     })
+    
+    # You can access the value of the widget with input$slider1, e.g.
+    output$value <- renderPrint({ input$slider1 })
+    
+    # You can access the values of the second widget with input$slider2, e.g.
+    output$range <- renderPrint({ input$slider2 })
+    
+    
   }
   
   shinyApp(ui, server)
